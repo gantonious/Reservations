1d2f6fcaaa1d09ecddf6b6ec459ef5bc7729af8c
George Antonious <george@antonious.ca>
2018-09-29 15:32:53 -0400

    Get migrations working

---
+++ Reservations.Database/DesignTimeReservationsContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Reservations.Database
+{
+    public class DesignTimeReservationsContextFactory : IDesignTimeDbContextFactory<ReservationsContext>
+    {
+        private const string CONNECTION_STRING_ENV_VARIABLE = "RESERVATIONS_CONNECTION_STRING";
+        
+        public ReservationsContext CreateDbContext(string[] args)
+        {
+            var connectionString = Environment.GetEnvironmentVariable(CONNECTION_STRING_ENV_VARIABLE);
+            var dbContextOptions = new DbContextOptionsBuilder()
+                .UseNpgsql(connectionString).Options;
+            
+            return new ReservationsContext(dbContextOptions);
+        }
+    }
+}
---
+++ Reservations.Database/Migrations/20180929193224_Reservations-0.1.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Reservations.Database.Migrations
+{
+    public partial class Reservations01 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Extras",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    GuestId = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Extras", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Guests",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Status = table.Column<string>(nullable: true),
+                    TotalGuests = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Guests", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Extras");
+
+            migrationBuilder.DropTable(
+                name: "Guests");
+        }
+    }
+}
