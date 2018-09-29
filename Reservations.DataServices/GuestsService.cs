475aa58ef95fe64d7268cc649ed70a54325af0eb
George Antonious <george@antonious.ca>
2018-09-29 16:47:25 -0400

    Add more methods to GuestService

--- Reservations.DataServices/GuestsService.cs
+++ Reservations.DataServices/GuestsService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Reservations.Database;
 using Reservations.Database.Models;
 using Reservations.DataServices.Models;
@@ -17,29 +18,48 @@ namespace Reservations.DataServices
             _reservationsContext = reservationsContext;
         }
 
-        public Guest SearchFor(string name)
+        public IEnumerable<Guest> GetAllGuests()
         {
-            var guest = _reservationsContext.Guests
-                .FirstOrDefault(g => g.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
-            
-            if (guest == null)
-            {
-                return null;
-            }
+            return _reservationsContext.Guests
+                .Select(g => BuildGuestFrom(g, GetExtrasForGuest(g.Id)));
+        }
 
-            var extras = _reservationsContext.Extras
-                .Where(e => e.GuestId == guest.Id);
-
-            return BuildGuestFrom(guest, extras);
+        public async Task<Guest> GetGuest(string id)
+        {
+            return await _reservationsContext.Guests
+                .Where(g => g.Id == id)
+                .Select(g => BuildGuestFrom(g, GetExtrasForGuest(g.Id)))
+                .FirstOrDefaultAsync();
         }
         
+        public async Task<Guest> SearchFor(string name)
+        {
+            return await _reservationsContext.Guests
+                .Where(g => g.Name.Equals(name, StringComparison.OrdinalIgnoreCase))
+                .Select(g => BuildGuestFrom(g, GetExtrasForGuest(g.Id)))
+                .FirstOrDefaultAsync();
+        }
+
         public async Task AddGuestAsync(string name, int maxExtras)
         {
             var newGuest = BuildGuest(name, maxExtras);
             await _reservationsContext.Guests.AddAsync(newGuest);
             await _reservationsContext.SaveChangesAsync();
         }
-            
+
+        public async Task DeleteGuestAsync(string id)
+        {
+            var guest = _reservationsContext.Guests.First(g => g.Id == id);
+            _reservationsContext.Guests.Remove(guest);
+            await _reservationsContext.SaveChangesAsync();
+        }
+
+        private IEnumerable<ExtraTableEntry> GetExtrasForGuest(string guestId)
+        {
+            return _reservationsContext.Extras
+                .Where(e => e.GuestId == guestId);
+        }
+        
         private static GuestTableEntry BuildGuest(string name, int maxExtras)
         {
             return new GuestTableEntry
