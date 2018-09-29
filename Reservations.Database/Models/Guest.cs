47fe6ab110a6423d8463a457c368b6b997d0a0eb
George Antonious <george@antonious.ca>
2018-09-29 14:39:36 -0400

    Start scafolding database project

---
+++ Reservations.Database/Models/Guest.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Reservations.Database.Models
+{
+    public class Guest
+    {
+        public string Id { get; set; }
+        public string InviteeId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Reservations.Database/Models/Invitee.cs
@@ -0,0 +1,9 @@
+﻿namespace Reservations.Database.Models
+{
+    public class Invitee
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int TotalGuests { get; set; } = 0;
+    }
+}
