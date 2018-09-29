3732393bba66f7ee36895a5da122a55c34174249
George Antonious <george@antonious.ca>
2018-09-29 15:51:40 -0400

    Scaffold DataServices

---
+++ Reservations.DataServices/GuestsService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Reservations.Database;
+
+namespace Reservations.DataServices
+{
+    public class GuestsService
+    {
+        private readonly ReservationsContext _reservationsContext;
+
+        public GuestsService(ReservationsContext reservationsContext)
+        {
+            _reservationsContext = reservationsContext;
+        }
+    }
+}
