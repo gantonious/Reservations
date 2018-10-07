8e2527e1b4d558661ef779ccad0855c79c903454
George Antonious <george@antonious.ca>
2018-10-07 09:04:13 -0400

    Wrap API inside client

---
+++ Reservations.Sdk/ReservationsClient.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Reservations.Sdk.Rest;
+
+namespace Reservations.Sdk
+{
+    public class ReservationsClient
+    {
+        private readonly IAdminApi _adminApi;
+
+        public ReservationsClient(IAdminApi adminApi)
+        {
+            _adminApi = adminApi;
+        }
+
+        public async Task<IEnumerable<Guest>> GetGuestsAsync()
+        {
+            return await _adminApi.GetGuestsAsync();
+        }
+        
+        public async Task AddGuestAsync(AddGuestRequest addGuestRequest)
+        {
+            await _adminApi.AddGuestAsync(addGuestRequest);
+        }
+        
+        public async Task AddGuestsAsync(IEnumerable<AddGuestRequest> addGuestRequests)
+        {
+            await _adminApi.AddGuestsAsync(addGuestRequests);
+        }
+
+        public async Task DeleteAllGuestsAsync()
+        {
+            await _adminApi.DeleteAllGuestsAsync();
+        }
+    }
+}
